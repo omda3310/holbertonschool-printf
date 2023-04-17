722d6b93675d775315f456be8ac5e9795589c20e
omda3310 <6197@holbertonstudents.com>
2023-04-17 11:16:15 +0200

    add main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MY_PRINTF
+#define MY_PRINTF
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * struct selector - struct selector
+ * @c: caracter
+ * @f: function associated
+ *
+ */
+typedef struct selector
+{
+	char *c;
+	int (*f)(va_list);
+} selec;
+int _printf(const char *format, ...);
+int print_c(va_list params);
+int print_s(va_list params);
+int print_percent(va_list params);
+int (*select_function(char c))(va_list);
+#endif
