850c445ed73ee217cc08f97d3ae5d3a9320a005c
omda3310 <6197@holbertonstudents.com>
2023-04-25 01:58:03 +0200

    fixed

--- functions.c
+++ functions.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include "main.h"
 /**
  * print_c - print caracter
  * @params: caracter
- * Return: caracter
+ * Return: int
  */
 int print_c(va_list params)
 {
 	int c;
 
 	c = va_arg(params, int);
-	return (putchar(c));
+	return (_putchar(c));
+	return (1);
 }
 /**
  * print_s - print string
@@ -30,7 +32,7 @@ int print_s(va_list params)
 	}
 	while (s[i] != '\0')
 	{
-		putchar(s[i]);
+		_putchar(s[i]);
 		i++;
 		c++;
 	}
@@ -43,92 +45,7 @@ int print_s(va_list params)
  */
 int print_percent(va_list params)
 {
-	char *s;
-	s = "%";
-	if (*s == va_arg(params, int))
-	{
-		return (*s);
-	}
-	return (*s);
-}
-/**
- * print_d - prints a decimal
- * @args: decimal argument
- * Return: counter
- */
-/*int print_d(va_list params)
-{
-	int val = va_arg(params, int);
-	printf("%d", val);
-	return val;
-}*/
-int print_d(va_list params)
-{
-
-	unsigned int absolute, aux, count, countnum;
-	int n;
-
-	count = 0;
-	n = va_arg(params, int);
-	if (n < 0)
-	{
-		absolute = (n * -1);
-		count += putchar('-');
-	}
-	else
-		absolute = n;
-
-	aux = absolute;
-	countnum = 1;
-	while (aux > 9)
-	{
-		aux /= 10;
-		countnum *= 10;
-	}
-	while (countnum >= 1)
-	{
-		count += putchar(((absolute / countnum) % 10) + '0');
-		countnum /= 10;
-	}
-	return (count);
-}
-/**
- * print_i - prints integer
- * @args: integer argument
- * Return: the decimal function
- */
-
-int print_i(va_list params)
-{
-	return (print_d(params));
-}
-int print_u(va_list params)
-{
-	unsigned int n = va_arg(params, unsigned int);
-	printf("%u", n);
-	return (1);
-}
-int print_o(va_list params)
-{
-	unsigned int n = va_arg(params, unsigned int);
-	printf("%o", n);
-	return (1);
-}
-int print_X(va_list params)
-{
-	int n = va_arg(params, int);
-	printf("%X", n);
-	return (1);
-}
-int print_x(va_list params)
-{
-	unsigned int n = va_arg(params, unsigned int);
-	printf("%x", n);
-	return (1);
-}
-int print_p(va_list params)
-{
-	int *n = va_arg(params, int *);
-	printf("%p", n);
+	(void)params;
+	_putchar('%');
 	return (1);
 }
