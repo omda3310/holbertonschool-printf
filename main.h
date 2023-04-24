b6cfb5f58c057d3d7fdd87af3873ab9dfebcde99
omda3310 <6197@holbertonstudents.com>
2023-04-25 01:57:06 +0200

    fixed

--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define MY_PRINTF
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 /**
  * struct selector - struct selector
  * @c: caracter
@@ -10,7 +11,7 @@
  */
 typedef struct selector
 {
-	char *c;
+	char c;
 	int (*f)(va_list);
 } selec;
 int _printf(const char *format, ...);
@@ -18,11 +19,14 @@ int print_c(va_list params);
 int print_s(va_list params);
 int print_d(va_list params);
 int print_i(va_list params);
+/**
 int print_u(va_list params);
 int print_p(va_list params);
 int print_x(va_list params);
 int print_X(va_list params);
 int print_o(va_list params);
+*/
 int print_percent(va_list params);
-int (*select_function(char c))(va_list);
+int _putchar(char c);
+int select_function(const char c, va_list params);
 #endif
