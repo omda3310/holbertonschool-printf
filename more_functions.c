f92891ef7ffbf21263b2da245234cdc024bab5b6
omda3310 <6197@holbertonstudents.com>
2023-04-25 02:00:15 +0200

    fixed

--- more_functions.c
+++ more_functions.c
@@ -47,34 +47,3 @@ int print_i(va_list params)
 {
     return (print_d(params));
 }
-/**
-int print_u(va_list params)
-{
-    unsigned int n = va_arg(params, unsigned int);
-    printf("%u", n);
-    return (1);
-}
-int print_o(va_list params)
-{
-    unsigned int n = va_arg(params, unsigned int);
-    printf("%o", n);
-    return (1);
-}
-int print_X(va_list params)
-{
-    unsigned int n = va_arg(params, unsigned int);
-    printf("%X", n);
-    return (1);
-}
-int print_x(va_list params)
-{
-    unsigned int n = va_arg(params, unsigned int);
-    printf("%x", n);
-    return (1);
-}
-int print_p(va_list params)
-{
-    int *n = va_arg(params, int *);
-    printf("%p", n);
-    return (1);
-}*/
